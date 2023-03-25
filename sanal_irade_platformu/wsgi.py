0456d487abbf126e24ce53960735f0cd382c3e93
Berkay MIZRAK <mizrakby@gmail.com>
2023-03-25 15:38:05 +0300

    feat: initialize django

---
+++ sanal_irade_platformu/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for sanal_irade_platformu project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sanal_irade_platformu.settings')
+
+application = get_wsgi_application()
