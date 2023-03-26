58118933ec8b2249619414ca581229ed5b98afa4
Berkay MIZRAK <mizrakby@gmail.com>
2023-03-26 03:49:45 +0300

    feat: create Abstract Model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class AbstractModel(models.Model):
+    updated_date = models.DateTimeField(
+        verbose_name='Updated Date',
+        blank=True,
+        auto_now=True,
+    )
+    created_date = models.DateTimeField(
+        verbose_name='Created Date',
+        blank=True,
+        auto_now_add=True,
+    )
+
+    class Meta:
+        abstract = True
