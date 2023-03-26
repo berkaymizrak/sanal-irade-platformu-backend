9538d75e798cdd3cbbbd1b9a82e24fe2debfdd32
Berkay MIZRAK <mizrakby@gmail.com>
2023-03-26 17:20:52 +0300

    feat: create city town district models

--- tools/admin.py
+++ tools/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from tools.models import *
+
 
 # Register your models here.
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code')
+    search_fields = ('name', 'code')
+    list_editable = []
+
+    class Meta:
+        model = City
+
+
+@admin.register(Town)
+class TownAdmin(admin.ModelAdmin):
+    list_display = ('name', 'city')
+    search_fields = ('name', 'city')
+    list_editable = []
+
+    class Meta:
+        model = Town
+
+
+@admin.register(District)
+class DistrictAdmin(admin.ModelAdmin):
+    list_display = ('name', 'postal_code', 'town')
+    search_fields = ('name', 'postal_code', 'town')
+    list_editable = []
+
+    class Meta:
+        model = District
--- tools/models.py
+++ tools/models.py
@@ -1,3 +1,67 @@
+from core.models import AbstractModel
 from django.db import models
 
+
 # Create your models here.
+
+class City(AbstractModel):
+    name = models.CharField(
+        max_length=255,
+        verbose_name='Name',
+    )
+    code = models.CharField(
+        max_length=5,
+        verbose_name='Code',
+    )
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'City'
+        verbose_name_plural = 'Cities'
+
+    def __str__(self):
+        return self.name
+
+
+class Town(AbstractModel):
+    name = models.CharField(
+        max_length=255,
+        verbose_name='Name',
+    )
+    city = models.ForeignKey(
+        City,
+        on_delete=models.CASCADE,
+        verbose_name='City',
+    )
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Town'
+        verbose_name_plural = 'Towns'
+
+    def __str__(self):
+        return self.name
+
+
+class District(AbstractModel):
+    name = models.CharField(
+        max_length=255,
+        verbose_name='Name',
+    )
+    postal_code = models.CharField(
+        max_length=5,
+        verbose_name='Postal code',
+    )
+    town = models.ForeignKey(
+        Town,
+        on_delete=models.CASCADE,
+        verbose_name='Town',
+    )
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'District'
+        verbose_name_plural = 'Districts'
+
+    def __str__(self):
+        return self.name
