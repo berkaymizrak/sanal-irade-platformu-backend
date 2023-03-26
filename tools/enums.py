c11c3e292fbd0d92110e5389a580cb97e6ea70a1
Berkay MIZRAK <mizrakby@gmail.com>
2023-03-27 01:35:38 +0300

    refactor: quotes

--- tools/enums.py
+++ tools/enums.py
@@ -3,8 +3,8 @@ from django.utils.translation import gettext_lazy as _
 
 
 class SocialMediaIcons(TextChoices):
-    FACEBOOK = "facebook", _("Facebook")
-    TWITTER = "twitter", _("Twitter")
-    GITHUB = "github", _("Github")
-    INSTAGRAM = "instagram", _("Instagram")
-    TELEGRAM = "telegram", _("Telegram")
+    FACEBOOK = 'facebook', _('Facebook')
+    TWITTER = 'twitter', _('Twitter')
+    GITHUB = 'github', _('Github')
+    INSTAGRAM = 'instagram', _('Instagram')
+    TELEGRAM = 'telegram', _('Telegram')
