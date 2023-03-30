13ee9f5b57651d5ef89e5d3f19578c2f71de43a4
Berkay MIZRAK <mizrakby@gmail.com>
2023-03-30 08:40:19 +0300

    feat: improve performance on admin

--- tools/admin.py
+++ tools/admin.py
@@ -8,15 +8,19 @@ from parler.admin import TranslatableAdmin
 @admin.register(City)
 class CityAdmin(TranslatableAdmin):
     list_display = ('name', 'language_column', 'code', 'updated_date', 'created_date',)
+    list_filter = ()
     search_fields = ('translations__name', 'code',)
     list_editable = ()
+    autocomplete_fields = ()
 
 
 @admin.register(Town)
 class TownAdmin(TranslatableAdmin):
     list_display = ('name', 'language_column', 'city', 'updated_date', 'created_date',)
+    list_filter = ()
     search_fields = ('translations__name', 'city__translations__name',)
     list_editable = ()
+    autocomplete_fields = ('city',)
 
     class Meta:
         model = Town
@@ -25,8 +29,10 @@ class TownAdmin(TranslatableAdmin):
 @admin.register(District)
 class DistrictAdmin(TranslatableAdmin):
     list_display = ('name', 'language_column', 'postal_code', 'town', 'updated_date', 'created_date',)
+    list_filter = ()
     search_fields = ('translations__name', 'postal_code', 'town__translations__name', 'town__city__translations__name',)
     list_editable = ()
+    autocomplete_fields = ('town',)
 
     class Meta:
         model = District
